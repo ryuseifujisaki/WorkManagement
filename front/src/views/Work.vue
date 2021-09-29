76841d9b07a2821cf5b9723c0c8c60ed52f43089
ryuseifujisaki <fujiryu1122@gmail.com>
2021-09-30 02:16:37 +0900

    front にapiを送るところまで

---
+++ front/src/views/Work.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+<h1>Work</h1>
+</div>
+</template>
+
+<script>
+import axios from "axios"
+export  default{
+
+  data(){
+
+  },
+  mounted: function(){
+    const url = process.env.VUE_APP_URL;
+    axios.get(url + "/works",{
+     headers: {
+          "Content-Type": "application/json",
+          "access-token": localStorage.getItem("access-token"),
+          client: localStorage.getItem("client"),
+          uid: localStorage.getItem("uid"),
+        },
+    })
+    .then((response) =>{
+    console.log(response);
+    })
+    .then((error)=>{
+      console.log(url);
+      console.log(error);
+    });
+  },
+};
+</script>
