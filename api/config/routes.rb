e0f95c2d97dc85354329659ebdda666ce80755ba
ryuseifujisaki <fujiryu1122@gmail.com>
2021-10-22 17:58:57 +0900

    finish delete機能

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :user_works
   resources :works
 
+  put "user_work/cancell" =>"user_works#cancell"
+
   namespace 'api' do
     namespace 'auth' do 
       get "registrations" => "registrations#account_update_params"
@@ -13,6 +15,7 @@ Rails.application.routes.draw do
       get "current_user/new" => "current_user_api#new"
       get "current_user/get_user_detail" => "current_user_api#get_user_detail"
       get "work_user/get_work_user/:id" => "work_user_api#get_work_user"
+      
     end
 
   end
