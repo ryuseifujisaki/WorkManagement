3f36c3ccfaa5703ce8151f89da4b50bf62a457e5
ryuseifujisaki <fujiryu1122@gmail.com>
2021-10-23 16:04:38 +0900

    fix

--- api/db/fixtures/user_seed.rb
+++ api/db/fixtures/user_seed.rb
@@ -3,24 +3,28 @@ User.seed(:id,
     id:1,
     name: "taro",
     email: "taro@email.com",
-    password: "password"
+    password: "password",
+    uid: "taro@email.com"
   },
   {
     id:2,
     name: "jiro",
-    email: "tjiroaro@email.com",
-    password: "password"
+    email: "jiro@email.com",
+    password: "password",
+    uid: "jiro@email.com"
   },
   {
     id:3,
     name: "saburo",
     email: "saburo@email.com",
-    password: "password"
+    password: "password",
+    uid: "saburo@email.com"
   },
   {
     id:4,
     name: "shiro",
     email: "shiro@email.com",
-    password: "password"
+    password: "password",
+    uid: "shiro@email.com"
   },
-)
+  )
