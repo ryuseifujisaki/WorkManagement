e37b7199f8603e57098f8d7e6279a763b777cb7c
ryuseifujisaki <fujiryu1122@gmail.com>
2021-11-11 00:50:57 +0900

    [finish] カラムの追加及び登録画面

--- api/db/fixtures/user_seed.rb
+++ api/db/fixtures/user_seed.rb
@@ -4,34 +4,45 @@ User.seed(:id,
     name: "taro",
     email: "taro@email.com",
     password: "password",
-    uid: "taro@email.com"
+    uid: "taro@email.com",
+    grade: "B3",
+    course: "環境社会基盤工学課程"
   },
   {
     id:2,
     name: "jiro",
     email: "jiro@email.com",
     password: "password",
-    uid: "jiro@email.com"
+    uid: "jiro@email.com",
+    grade: "B2",
+    course: "情報・経営システム工学課程"
   },
   {
     id:3,
     name: "saburo",
     email: "saburo@email.com",
     password: "password",
-    uid: "saburo@email.com"
+    uid: "saburo@email.com",
+    grade: "M1",
+    course: "機械創造工学課程"
   },
   {
     id:4,
     name: "shiro",
     email: "shiro@email.com",
     password: "password",
-    uid: "shiro@email.com"
+    uid: "shiro@email.com",
+    grade: "B1",
+    course: "電気電子情報工学課程",
+    
   },
   {
     id:5,
     name: "test",
     email: "test@email.com",
     password: "password",
-    uid: "shiro@email.com"
+    uid: "shiro@email.com",
+    grade: "M2",
+    course: "原子力,システム安全,技ノベ,その他"
   },
   )
