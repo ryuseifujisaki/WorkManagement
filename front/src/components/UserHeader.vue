013480c09823be993c65a7fef59c8cda01feaa89
ryuseifujisaki <fujiryu1122@gmail.com>
2021-11-13 21:52:13 +0900

    [Finish] create user header

---
+++ front/src/components/UserHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card class="pa-12" color="green" flat>
+    <v-card elevation="12" width="256">
+      <v-navigation-drawer floating permanent>
+        <v-list dense rounded>
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            :href="item.link"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+  </v-card>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: "Work Information",
+          icon: "mdi-facebook-workplace",
+          link: "/work",
+        },
+        {
+          title: "User Information",
+          icon: "mdi-account",
+          link: "/userinformation",
+        },
+      ],
+    };
+  },
+};
+</script>
