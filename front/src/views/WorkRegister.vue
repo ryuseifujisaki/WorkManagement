d2f2fc547ac314a6eb3c8c10b1bcf6d306161a4d
ryuseifujisaki <fujiryu1122@gmail.com>
2021-11-21 14:25:06 +0900

    [FIX] work register page

--- front/src/views/WorkRegister.vue
+++ front/src/views/WorkRegister.vue
@@ -9,35 +9,41 @@
       <v-row>
         <v-col cols="3"></v-col>
         <v-col cols="6">
-          <v-form ref="form" lazy-validation>
-            <v-text-field label="Work_name" v-model="name" :rules="rules">
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              label="Work_name"
+              v-model="name"
+              :rules="rules"
+              required
+            >
             </v-text-field>
             <v-text-field
               label="Number of people"
               v-model="limit"
               :rules="rules"
+              required
             >
             </v-text-field>
-            <v-text-field label="Day" v-model="day" :rules="rules">
+            <v-text-field label="Day" v-model="day" :rules="rules" required>
             </v-text-field>
-            <v-text-field label="money" v-model="money" :rules="rules">
+            <v-text-field label="money" v-model="money" :rules="rules" required>
             </v-text-field>
-            <v-text-field label="Where" v-model="where" :rules="rules">
+            <v-text-field label="Where" v-model="where" :rules="rules" required>
             </v-text-field>
-            <v-text-field label="Carfare" v-model="carfare"> </v-text-field>
-            <v-text-field label="content" v-model="content" :rules="rules">
+            <v-text-field label="Carfare" v-model="carfare" required>
             </v-text-field>
-            <v-text-field label="cloth" v-model="cloth" :rules="rules">
+            <v-text-field
+              label="content"
+              v-model="content"
+              :rules="rules"
+              required
+            >
+            </v-text-field>
+            <v-text-field label="cloth" v-model="cloth" :rules="rules" required>
             </v-text-field>
             <v-text-field label="belonging" v-model="belonging"> </v-text-field>
             <v-text-field label="other" v-model="other"> </v-text-field>
-            <v-btn
-              @click="
-                validate();
-                register;
-              "
-              >register</v-btn
-            >
+            <v-btn @click="register()">register</v-btn>
           </v-form>
         </v-col>
         <v-col cols="3"></v-col>
@@ -63,7 +69,7 @@ export default {
       belonging: "",
       other: "",
       flag: null,
-      valid: true,
+      valid: false,
       //rule規則
       rules: [(v) => !!v || "必須項目"],
     };
@@ -73,37 +79,40 @@ export default {
   },
   methods: {
     register: function () {
-      const url = process.env.VUE_APP_URL;
-      var params = {
-        name: this.name,
-        limit: this.limit,
-        day: this.day,
-        money: this.money,
-        where: this.where,
-        carfare: this.carfare,
-        content: this.content,
-        cloth: this.cloth,
-        belonging: this.belonging,
-        other: this.other,
-      };
-      axios
-        .post(url + "/works", params, {
-          headers: {
-            "Content-Type": "application/json",
-            "access-token": localStorage.getItem("access-token"),
-            client: localStorage.getItem("client"),
-            uid: localStorage.getItem("uid"),
-          },
-        })
-        .then((response) => {
-          console.log("succece");
-          console.log(response);
-          location.reload();
-        });
+      if (this.valid == true) {
+        const url = process.env.VUE_APP_URL;
+        var params = {
+          name: this.name,
+          limit: this.limit,
+          day: this.day,
+          money: this.money,
+          where: this.where,
+          carfare: this.carfare,
+          content: this.content,
+          cloth: this.cloth,
+          belonging: this.belonging,
+          other: this.other,
+        };
+        axios
+          .post(url + "/works", params, {
+            headers: {
+              "Content-Type": "application/json",
+              "access-token": localStorage.getItem("access-token"),
+              client: localStorage.getItem("client"),
+              uid: localStorage.getItem("uid"),
+            },
+          })
+          .then((response) => {
+            console.log("succece");
+            console.log(response);
+            location.reload();
+          });
+      }
     },
     //入力規制
     validate() {
       this.$refs.form.validate();
+      console.log(this.valid);
     },
   },
   mounted: function () {
@@ -136,6 +145,7 @@ export default {
         this.userName = response.data.data.name;
         console.log(this.userName);
       });
+    this.validate();
   },
 };
 </script>
