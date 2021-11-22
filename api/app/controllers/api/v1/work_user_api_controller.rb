721379ef712d97b19899170c8e110f6702fe6396
ryuseifujisaki <fujiryu1122@gmail.com>
2021-11-23 01:45:58 +0900

    [fix]

--- api/app/controllers/api/v1/work_user_api_controller.rb
+++ api/app/controllers/api/v1/work_user_api_controller.rb
@@ -12,4 +12,41 @@ class Api::V1::WorkUserApiController < ApplicationController
     end
     render json:  usersname
   end
+
+  def get_work_detail
+    @work = Work.all
+    workdetails = []
+    for work in @work
+      usersname  = []
+      work_name = work.name
+      work_limit = work.limit
+      work_day = work.day
+      work_where = work.where
+      work_money = work.money
+      work_carfare = work.carfare
+      work_content = work.content
+      work_cloth = work.cloth
+      work_belonging = work.belonging
+      work_other = work.other
+      work_users = work.users
+      for work_user in work_users 
+        user_name = work_user.name
+        usersname.push(user_name)
+      end
+      workdetails << {
+        name: work_name,
+        limit: work_limit,
+        day: work_day,
+        where: work_where,
+        money: work_money,
+        carfare: work_carfare,
+        content: work_content,
+        cloth: work_cloth,
+        belonging: work_belonging,
+        other: work_other,
+        usernames: usersname,
+      }
+     end
+      render json: workdetails
+  end
 end
